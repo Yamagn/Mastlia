9a0a94fee7401d85d64d6239c09569e5557465f1
Yamagn <syanekenpon@gmail.com>
2018-11-23 17:17:56 +0900

    storyboardの追加

---
+++ Mastlia/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Mastlia
+//
+//  Created by ymgn on 2018/11/23.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
