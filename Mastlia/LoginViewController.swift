37503d237249426eded92ccb069c5c65ad41eb02
Yamagn <syanekenpon@gmail.com>
2018-11-26 23:56:05 +0900

    タイムラインの取得

--- Mastlia/LoginViewController.swift
+++ Mastlia/LoginViewController.swift
@@ -8,34 +8,59 @@
 
 import UIKit
 import MastodonKit
+import RealmSwift
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var domain: UITextField!
     @IBOutlet weak var mail: UITextField!
     @IBOutlet weak var pass: UITextField!
     var responseJson = Dictionary<String, AnyObject>()
+    var user: Account = Account()
     
     @IBAction func onLoginButtonTapped(_ sender: Any) {
-        if domain == nil {
+        if domain.text == nil || mail == nil || pass == nil {
             let controller = UIAlertController(title: nil, message: "正しく入力してください", preferredStyle: .alert)
             controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             present(controller, animated: true, completion: nil)
         }
+        
+        
         let client = Client(baseURL: "https://" + domain.text!)
         
         let request = Clients.register(clientName: "Mastlia", scopes: [.read, .write, .follow], website: "https://github.com/MastodonKit/MastodonKit")
         client.run(request) { result in
-            if let application = result.value {
-                print("id: \(application.id)")
-                print("redirect uri: \(application.redirectURI)")
-                print("client id: \(application.clientID)")
-                print("client secret: \(application.clientSecret)")
+            guard let application = result.value else {
+                let controller = UIAlertController(title: nil, message: "入力された内容が正しくありませんでした", preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+                return
+            }
+            let loginRequest = Login.silent(clientID: application.clientID, clientSecret: application.clientSecret, scopes: [.read, .write], username: self.mail.text!, password: self.pass.text!)
+            client.run(loginRequest) { result in
+                if let loginSettings = result.value {
+                    DispatchQueue.main.async {
+                        self.user.domain = self.domain.text!
+                        self.user.mail = self.mail.text!
+                        self.user.pass = self.pass.text!
+                        self.user.accessToken = loginSettings.accessToken
+                        self.save(user: self.user)
+                    }
+                }
             }
         }
     }
-    
-    
-    
+    func save(user: Account) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(user)
+            }
+        } catch {
+            let controller = UIAlertController(title: nil, message: "エラーが発生しました", preferredStyle: .alert)
+            controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- Mastlia/TimeLineViewController.swift
+++ Mastlia/TimeLineViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import RealmSwift
+import MastodonKit
 
 class TimeLineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
-    var dataList: [Toots] = []
+    
+    var user: Account = Account()
+    var dataList: [Status] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let realm = try! Realm()
+        let users = realm.objects(Account.self)
+        user = users[0]
+        if users.count == 0 {
+            performSegue(withIdentifier: "moveLoginView", sender: nil)
+            return
+        }
 
         let tootNib = UINib(nibName: "TootCell", bundle: nil)
         tableView.register(tootNib, forCellReuseIdentifier: "TootCell")
@@ -21,7 +33,23 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func reloadListDatas() {
-        
+        let req = Timelines.home(range: .default)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let client = Client(baseURL: "https://" + self.user.domain, accessToken: self.user.accessToken, session: session)
+        client.run(req) { result in
+            if let statuses = result.value {
+                self.dataList = statuses
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } else {
+                let controller = UIAlertController(title: nil, message: "タイムラインを取得できませんでした", preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+                return
+            }
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -33,15 +61,16 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TootCell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! TootCell
-        
+        let cell: TootCell = tableView.dequeueReusableCell(withIdentifier: "TootCell", for: indexPath) as! TootCell
         let data = dataList[indexPath.row]
-        cell.retCount.text = String(data.reblogs_count)
-        cell.repCount.text = String(data.replies_count)
-        cell.favCount.text = String(data.favorites_count)
+        print(data)
+        cell.retCount.text = String(data.reblogsCount)
+        cell.repCount.text = String(data.mentions.count)
+        cell.favCount.text = String(data.favouritesCount)
         cell.tootContent.text = data.content
         cell.userID.text = data.account.username
-        cell.userName.text = data.account.display_name
+        cell.userName.text = data.account.displayName
+        print(data.account.avatar)
         cell.userImage.setImage(fromUrl: data.account.avatar)
         
         return cell
@@ -49,8 +78,8 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
 }
 
 extension UIImageView {
-    public func setImage(fromUrl url: URL) {
-        URLSession.shared.dataTask(with: URLRequest(url: url)) {(data, response, error) in
+    public func setImage(fromUrl url: String) {
+        URLSession.shared.dataTask(with: URLRequest(url: URL(string: url)!)) {(data, response, error) in
             guard let data = data, let _ = response, error == nil else {
                 return
             }
