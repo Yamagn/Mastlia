3f6b09db23fee81e6acfe33ca651f8031f2fe492
Yamagn <syanekenpon@gmail.com>
2018-12-04 08:49:00 +0900

    トゥートの詳細画面を設置(未検証)

--- Mastlia/Controllers/DetailViewController.swift
+++ Mastlia/Controllers/DetailViewController.swift
@@ -7,84 +7,101 @@
 //
 
 import UIKit
+import MastodonKit
 
-class DetailViewController: UITableViewController {
-
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var detailTable: UITableView!
+    var replys: [Mention] = []
+    var catchToot: Status? = nil
+    var user: Account = Account()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let mainNib = UINib(nibName: "DetailCell", bundle: nil)
+        let countNib = UINib(nibName: "ActivityCountCell", bundle: nil)
+        let activityNib = UINib(nibName: "ActivityCell", bundle: nil)
+        let tootNib = UINib(nibName: "TootCell", bundle: nil)
+        
+        detailTable.register(mainNib, forCellReuseIdentifier: "DetailCell")
+        detailTable.register(countNib, forCellReuseIdentifier: "ActivityCountCell")
+        detailTable.register(activityNib, forCellReuseIdentifier: "ActivityCell")
+        detailTable.register(tootNib, forCellReuseIdentifier: "TootCell")
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3 + replys.count
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        formatter.locale = Locale(identifier: "ja_JP")
+        guard let toot = catchToot  else{
+            let cell = UITableViewCell()
+            return cell
+        }
+        replys = toot.mentions
+        switch indexPath.row {
+        case 0:
+            let cell: DetailCell = tableView.dequeueReusableCell(withIdentifier: "DetailCell") as! DetailCell
+            cell.userAvater.setImage(fromUrl: toot.account.avatar)
+            cell.userID.text = toot.account.id
+            cell.userName.text = toot.account.displayName
+            cell.dateTime.text = formatter.string(from: toot.createdAt)
+            let attributedString = NSAttributedString.parseHTML2Text(sourceText: "<b><font size=5>" + toot.content + "</b>")
+            cell.tootContent.attributedText = attributedString
+            return cell
+        case 1:
+            let cell: ActivityCountCell = tableView.dequeueReusableCell(withIdentifier: "ActivityCountCell") as! ActivityCountCell
+            if toot.reblogsCount > 0 {
+                cell.boostCount.text = String(toot.reblogsCount)
+            } else {
+                cell.boostCount.isHidden = true
+            }
+            
+            if toot.favouritesCount > 0 {
+                cell.favCount.text = String(toot.favouritesCount)
+            } else {
+                cell.favCount.isHidden = true
+            }
+            return cell
+        case 2:
+            let cell: ActivityCell = tableView.dequeueReusableCell(withIdentifier: "ActivityCell") as! ActivityCell
+            
+            return cell
+        default:
+            let cell: TootCell = tableView.dequeueReusableCell(withIdentifier: "TootCell", for: indexPath) as! TootCell
+            
+            let data = replys[indexPath.row - 3]
+            print(data)
+//            cell.retCount.text = String(data.reblogsCount)
+//            cell.repCount.text = String(data.mentions.count)
+//            cell.favCount.text = String(data.favouritesCount)
+//            //        cell.tootContent.text = data.content
+//            let attributedString = NSAttributedString.parseHTML2Text(sourceText: "<b><font size=5>" + data.content + "</b>")
+//            cell.tootContent.attributedText = attributedString
+//            cell.userID.text = "@" + data.account.username
+//            cell.userName.text = data.account.displayName
+//            print(data.account.avatar)
+//            cell.userImage.setImage(fromUrl: data.account.avatar)
+//            cell.id = data.id
+//            cell.user.domain = self.user.domain
+//            cell.user.accessToken = self.user.accessToken
+//            cell.isFavorited = data.favourited ?? false
+//            cell.isRebloged = data.reblogged ?? false
+//            cell.judge()
+            
+            return cell
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
